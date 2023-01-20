c50fd7167ea8a17329e929bb4d36389f140be3c8
Vicente Vendramin <guesser.vicente@gmail.com>
2023-01-20 14:33:01 -0300

    Iniciando o projeto React Testing Library

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componenete App', () => {
+  it('Testa o conjunto de links de navegação', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+});
