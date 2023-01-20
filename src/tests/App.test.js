c0eeb2d9aa6ffa61fad240b0afe478ca749847b4
Vicente Vendramin <guesser.vicente@gmail.com>
2023-01-20 15:20:23 -0300

    Requisito 1: Teste o componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testa o componenete App', () => {
-  it('Testa o conjunto de links de navegação', () => {
-    renderWithRouter(<App />);
+  it('Ao clicar no link Home, direciona para a rota "/"', () => {
+    const { history } = renderWithRouter(<App />);
 
     const homeLink = screen.getByRole('link', { name: /Home/i });
-    const aboutLink = screen.getByRole('link', { name: /About/i });
-    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
-
     expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Ao clicar no link About, direciona para a rota "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /About/i });
     expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Ao clicar no link Pokémon Favoritados, direciona para a rota "/favorites"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
     expect(favoriteLink).toBeInTheDocument();
+    userEvent.click(favoriteLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
   });
 });
