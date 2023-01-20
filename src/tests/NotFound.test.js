ec971a055c23ae454f8371c8a82af742343f4b88
Vicente Vendramin <guesser.vicente@gmail.com>
2023-01-20 16:39:58 -0300

    Requisito 4: Implementa testes no componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  it('A página contém um heading com o text "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('A página mostra uma imagem correta', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
